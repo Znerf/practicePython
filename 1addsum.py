d53e88f0c7685886f6b1dbe1808f49ad565203a6
Sagar Shrestha <znerfii.a1@gmail.com>
2024-01-09 13:09:25 -0600

    feat: sum two

---
+++ 1addsum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        '''for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j] == target :
+                    return [i,j]
+        '''
+        hash_map = {}
+        for index, value in enumerate(nums):
+            hash_map[value] = index
+        for index1, value in enumerate(nums):
+            if target - value in hash_map:
+                index2 = hash_map[target - value]
+                if index1 != index2:
+                    return [index1 , index2 ]
