34e759919e961c2258201978bb02c596b075dc9a
Sagar <znerfii.a1@gmail.com>
2024-10-08 13:22:18 -0500

    feat: cycle in linked list

---
+++ 141. cycle in liunked list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast= fast.next.next
+            slow= slow.next
+            if fast == slow:return True
+        return False #o(n) o(1)
+        dic={}
+        while head: #o(nlog(n)) 0(n)
+            if head in dic: return 1
+            dic[head]=0
+            head = head.next
+        return 0
+
+        
+        
